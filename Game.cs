cc48703c011196ffdbb8620129947c5f2d1d2467
idiot <qpalqpzm999@gmail.com>
2024-07-08 13:54:20 +0700

    type 1 reproduction|neighbors complete 🥵😈

--- Game.cs
+++ Game.cs
@@ -10,47 +10,58 @@ namespace cocojambo {
         private Cell[,] field;
         private int field_size;
         public Game(int field_size) {
-            this.field_size = field_size; 
+            this.field_size = field_size;
             field = new Cell[field_size, field_size];
             for (int i = 0; i < field_size; i++)
                 for (int j = 0; j < field_size; j++)
                     field[i, j] = new Cell();
         }
-        
+
         public Cell[,] get_field() {
-            var field_copy = new Cell[field_size,field_size];
+            var field_copy = new Cell[field_size, field_size];
             for (int i = 0; i < field_size; i++)
                 for (int j = 0; j < field_size; j++)
-                    field_copy[i,j] = field[i, j];
-            return field_copy; 
+                    field_copy[i, j] = field[i, j];
+            return field_copy;
         }
         public void add_cell(int i, int j, int type) {
-            if (validate_pos(i,j))
-                field[i,j] = new Cell(type);
+            if (validate_pos(i, j))
+                field[i, j] = new Cell(type);
         }
         public void delete_cell(int i, int j) {
             if (validate_pos(i, j))
                 field[i, j] = new Cell();
         }
-        private bool validate_pos(int i, int j) { 
-            return i >= 0 && j >= 0 && i<=field_size && j<=field_size;
+        private bool validate_pos(int i, int j) {
+            return i >= 0 && j >= 0 && i <= field_size && j <= field_size;
         }
 
         private bool chance_result(int chance) {
             return random.Next(1, 101) <= chance;
         }
-        private Dictionary<int, int> neighbors(int i, int j) {
-
-            Dictionary<int, int> neighbors_cells = new Dictionary<int, int>();
+        public class Neighbors {
+            public int count;
+            public List<(int,int)> pos;
+            public Neighbors() {
+                count = 0;
+                pos = new List<(int,int)> ();
+            }
+        }
+        private Dictionary<int, Neighbors> neighbors(int i, int j) {
+            
+            Dictionary<int, Neighbors> neighbors_cells = new Dictionary<int,Neighbors>();
             for (int k = 0; k < 4; k++)
-                neighbors_cells[k] = 0;
+                neighbors_cells[k] = new Neighbors();
+
             for (int ii = -1; ii < 1; ii++)
                 for (int jj = -1; jj < 1; jj++) {
                     int col = (i + ii + field_size) % field_size;
                     int row = (j + jj + field_size) % field_size;
                     bool selected_cell = i == row && j == col;
-                    if (!selected_cell)
-                        neighbors_cells[field[col, row].type] += 1;
+                    if (!selected_cell) {
+                        neighbors_cells[field[col, row].type].count += 1;
+                        neighbors_cells[field[col, row].type].pos.Add((col, row));
+                    }
                 }
             return neighbors_cells;
         }
@@ -62,17 +73,26 @@ namespace cocojambo {
 
             for (int i = 0; i < field_size; i++)
                 for (int j = 0; j < field_size; j++) {
-                    Dictionary<int, int> neighbors_cells = new Dictionary<int, int>();
+                    Dictionary<int, Neighbors> neighbors_cells = new Dictionary<int, Neighbors>();
                     neighbors_cells = neighbors(i, j);
                     switch (field_copy[i,j].type) 
                     {
                         case 0: {
-
                                 if (chance_result(field_copy[i, j].reproduction_chance))
                                     field_copy[i, j] = new Cell(1);
                                 break;
                         }
-                    }
+                        case 1: {
+                                if (neighbors_cells[0].count > 0) 
+                                    if (chance_result(field_copy[i, j].reproduction_chance)) {
+                                        int rand_pos = random.Next(0, neighbors_cells[0].count);
+                                        int x = neighbors_cells[0].pos[rand_pos].Item1;
+                                        int y = neighbors_cells[0].pos[rand_pos].Item2;
+                                        field_copy[x,y] = new Cell(1);
+                                    }  
+                                break;
+                        }
+                    }   
                 }
             field = field_copy;
         }
